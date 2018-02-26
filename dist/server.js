69c023b8e9bb204a417392b580f01891c5bc6f56
potranco <chemar@gmail.com>
2018-02-26 20:03:38 +0100

    initial test

--- dist/server.js
+++ dist/server.js
@@ -8,14 +8,25 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express2.default)();
 var port = 3000;
+var router = _express2.default.Router();
 
 app.use('/assets', _express2.default.static('dist'));
-
 app.get('/', function (req, res) {
   var componente = 'server code';
   var html = '\n    <html>\n    <head>\n      <title>Server</title>\n    </head>\n    <body>\n      <h1>hola mundo</h1>\n      <div id="serverside">\n        ' + componente + '\n      </div>\n      <div id="app">Code client</div>\n      <script src="/assets/app.js"></script>\n    </body>\n    </html>\n  ';
   res.send(html);
 });
+
+router.get('/', function (req, res, next) {
+  res.send('peticion con router.get');
+  next();
+});
+
+router.get('/about', function (req, res, next) {
+  res.send('Peticon /about con router.get');
+});
+
+app.use('/', router);
 
 app.listen(port, function () {
   console.log('Example server app listening on port 3000!');
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import express from 'express'
 const app = express()
 const port = 3000
+const router = express.Router()
 
 app.use('/assets', express.static('dist'))
-
 app.get('/', function (req, res) {
   let componente = 'server code'
   const html = `
@@ -23,6 +23,17 @@ app.get('/', function (req, res) {
   `
   res.send(html)
 })
+
+router.get('/', function (req, res, next) {
+  res.send('peticion con router.get')
+  next()
+})
+
+router.get('/about', function (req, res, next) {
+  res.send('Peticon /about con router.get')
+})
+
+app.use('/', router)
 
 app.listen(port, function () {
   console.log('Example server app listening on port 3000!')
